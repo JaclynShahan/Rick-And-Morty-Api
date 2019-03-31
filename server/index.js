468995178bc2281e7c9195ddd57e5001bd3aafe6
Jaclyn <jaclyndshahan@gmail.com>
2019-03-31 14:56:49 -0500

    working front and back end

--- server/index.js
+++ server/index.js
@@ -8,5 +8,9 @@ app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/api/test', (req, res) => {
+    res.status(200).send({message: 'it works'})
+})
+
 const port = 3002;
 app.listen(port, () => console.log(`Server listening on port ${port}`))
